c5cad1133deaba9d2a7d1cd1a7ef1ea76f93f37e
Shivani Singh <97497268+shivani2190@users.noreply.github.com>
2025-02-17 23:08:40 +0530

    Update JobDetails.jsx

--- frontend/src/components/Job/JobDetails.jsx
+++ frontend/src/components/Job/JobDetails.jsx
@@ -12,7 +12,7 @@ const JobDetails = () => {
 
   useEffect(() => {
     axios
-      .get(`http://localhost:4000/api/v1/job/${id}`, {
+      .get(`https://job-portal-web-site-lz4y.onrender.com/api/v1/job/${id}`, {
         withCredentials: true,
       })
       .then((res) => {
