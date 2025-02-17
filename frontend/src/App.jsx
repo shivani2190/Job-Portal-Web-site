a7352913199b7e79726cf79a414e8f176ab9c542
Shivani Singh <97497268+shivani2190@users.noreply.github.com>
2025-02-17 23:11:57 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -22,7 +22,7 @@ const App = () => {
   useEffect(() => {
     const fetchUser = async () => {
       try {
-        const response = await axios.get("http://localhost:4000/api/v1/user/getuser", { withCredentials: true });
+        const response = await axios.get("https://job-portal-web-site-lz4y.onrender.com/api/v1/user/getuser", { withCredentials: true });
         setUser(response.data.user);
         setIsAuthorized(true);
       } catch (error) {
